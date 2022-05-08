08030183a72dd7381e4dc84ae47ecd859677178b
MFR096 <105156401+MFR096@users.noreply.github.com>
2022-05-08 13:27:08 +0100

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,37 @@
+if (localStorage.getItem('session')) {
+    window.location.replace("home.html");
+}
+
+function login_form() {
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    if (!username || !password) {
+        alert("All fields are requiered!");
+        return;
+    }
+
+    let users = [];
+    let localusers = JSON.parse(localStorage.getItem('users'));
+    if (localusers) users = localusers;
+
+    let crt_user = null;
+    let hash = CryptoJS.SHA512(password).toString(CryptoJS.enc.Base64);
+
+    //check if there is a user with the username and password
+    users.every(user => {
+        if ((user.username == username) && (user.pswrd == hash)) {
+            crt_user = user;
+            return;
+        }
+    });
+
+    if (crt_user) {
+        localStorage.setItem('session', 'valid');
+        localStorage.setItem('user', JSON.stringify(crt_user));
+        window.location.replace("home.html");
+    } else {
+        alert("The username or password is incorrect, try again!");
+        return;
+    }
+
+}
